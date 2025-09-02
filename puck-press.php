7c79f7caa2d9842835d3fb6dbce724240b61eaa2
unknown <connormesec@gmail.com>
2025-09-02 14:28:30 -0600

    updated versioning

--- puck-press.php
+++ puck-press.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Puck Press
  * Plugin URI:        https://www.google.com
  * Description:       This plugin hosts a number of tools for creating the best ice hockey website
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Connor Mesec
  * Author URI:        https://https://github.com/connormesec//
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PUCK_PRESS_VERSION', '1.0.0' );
+define( 'PUCK_PRESS_VERSION', '1.0.1' );
 define( 'PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
 
 /**
