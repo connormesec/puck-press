b5552f2a8c041e2fc7559e1e033a6deebbfc690d
connormesec <connormesec@gmail.com>
2026-01-29 10:02:20 -0700

    Enhance card stack with skater player cards
    Added functionality to build player cards for skaters without assigned positions in the card stack.

--- public/templates/roster-templates/cardstack_template.php
+++ public/templates/roster-templates/cardstack_template.php
@@ -61,6 +61,18 @@ class CardStackTemplate extends PuckPressTemplate
     public function buildCardStack(array $players)
     {
         $content = '<div class="cardstack_container clearfix">';
+        
+        // Skaters (players without assigned positions) - shown first
+        $skaters = $this->getPlayersWithoutPositions($players);
+        if (!empty($skaters)) {
+            $content .= '<div class="player_group">';
+            $content .= '<div class="player_position_title"><h2>Skaters</h2></div>';
+            foreach ($skaters as $player) {
+                $content .= $this->createPlayerCard($player['id'], $player['player_id'], $player['headshot_link'], $player['number'], $player['name'], $player['pos'], $player['hometown'], $player['ht'], $player['wt'], $player['shoots'], $player['year_in_school'] ?? null, $player['last_team'] ?? null, $player['major'] ?? null);
+            }
+            $content .= '</div>';
+        }
+        
         //forwards
         $content .= '<div class="player_group">';
         $content .= '<div class="player_position_title"><h2>Forwards</h2></div>';
